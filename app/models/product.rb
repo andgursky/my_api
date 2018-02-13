a3c8c59d0bb0246261d9e50e7638ea28c34a5204
Andrey Gursky <andgursky@gmail.com>
2018-02-13 15:36:55 +0200

    Product - model was implemented

---
+++ app/models/product.rb
@@ -0,0 +1,21 @@
+class Product < ApplicationRecord
+  validates :price, numericality: true
+  validates :name, presence: true, uniqueness: true
+  belongs_to :category
+  after_save :set_current_product_count
+  after_destroy :set_current_product_count
+  validate :non_zero_price
+
+  private
+
+  def set_current_product_count
+    category = Category.find(self.category_id.to_i)
+    Category.transaction do
+      category.update(products_count: category.products.count)
+    end
+  end
+
+  def non_zero_price
+    self.errors.add(:price, "should be grater then zero!") if self.price <= 0
+  end
+end
