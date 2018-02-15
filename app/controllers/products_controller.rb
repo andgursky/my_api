ce6bf4567335e9fc7edf7504daffd1104d317267
Andrey Gursky <andgursky@gmail.com>
2018-02-15 21:36:36 +0200

    ProductsController - was implemented
    actions:
            - index
            - create
            - delete

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+  def index
+    # GET /categories/:id/products
+    render json: Category.find(params["category_id"]).
+      products.as_json(only: [:id, :name, :price])
+  end
+
+  def create
+    # POST /categories/:id/products
+    category = Category.find(params["category_id"])
+    new_product = category.products.create(name:params["product"]["name"],
+                                           price:params["product"]["price"])
+    if new_product.errors.messages.any?
+      render status: 422,
+             json: new_product.errors.messages
+    else
+      render status: 201,
+             json: new_product.as_json(only: [:id, :name, :price])
+    end
+  end
+
+  def delete
+    # DELETE /products/:id
+    product = Product.find(params["id"]).destroy
+    unless product.errors.messages.any?
+      render status: 204
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #post '/create', action: 'create', controller: 'categories'
-  resources :categories, only: %i[create index]
+
+  delete '/products/:id' => 'products#delete'
+
+  resources :categories, only: %i[create index] do
+    #get ':id/products', action: :index,  controller: 'products'
+    #post ':id/products', action: :create,  controller: 'products'
+    resources :products, only: %i[index create]
+  end
 end
