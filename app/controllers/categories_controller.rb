25154a623f3a9e7585c33414da00b133485aa821
Andrey Gursky <andgursky@gmail.com>
2018-02-15 20:34:45 +0200

    CategoriesController - was implemented
    now we have two actions:
            - index ( render all categories )
            - create ( creates new category and render it's data as json)

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+  def index
+    # GET /categories
+    render status: 200,
+           json: Category.all.as_json(only: [:id, :name, :products_count])
+  end
+
+  def create
+    # POST /categories
+    new_category = Category.create(name:params["category"]["name"])
+    if new_category.errors.messages.any?
+      render status: 422,
+             json: new_category.errors.messages
+    else
+      render status: 201,
+             json: new_category.as_json(only: [:id, :name, :products_count])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #post '/create', action: 'create', controller: 'categories'
+  resources :categories, only: %i[create index]
 end
