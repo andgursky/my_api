0b77c65b1ce63d1d99c350be5d86e7acc40dc333
Andrey Gursky <andgursky@gmail.com>
2018-02-16 12:32:09 +0200

    Rubocop - gem was added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,18 +2,18 @@ class CategoriesController < ApplicationController
   def index
     # GET /categories
     render status: 200,
-           json: Category.all.as_json(only: [:id, :name, :products_count])
+           json: Category.all.as_json(only: %i[id name products_count])
   end
 
   def create
     # POST /categories
-    new_category = Category.create(name:params["category"]["name"])
+    new_category = Category.create(name: params['category']['name'])
     if new_category.errors.messages.any?
       render status: 422,
              json: new_category.errors.messages
     else
       render status: 201,
-             json: new_category.as_json(only: [:id, :name, :products_count])
+             json: new_category.as_json(only: %i[id name products_count])
     end
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,29 +1,27 @@
 class ProductsController < ApplicationController
   def index
     # GET /categories/:id/products
-    render json: Category.find(params["category_id"]).
-      products.as_json(only: [:id, :name, :price])
+    render json: Category.find(params['category_id'])
+      .products.as_json(only: %i[id name price])
   end
 
   def create
     # POST /categories/:id/products
-    category = Category.find(params["category_id"])
-    new_product = category.products.create(name:params["product"]["name"],
-                                           price:params["product"]["price"])
+    category = Category.find(params['category_id'])
+    new_product = category.products.create(name: params['product']['name'],
+                                           price: params['product']['price'])
     if new_product.errors.messages.any?
       render status: 422,
              json: new_product.errors.messages
     else
       render status: 201,
-             json: new_product.as_json(only: [:id, :name, :price])
+             json: new_product.as_json(only: %i[id name price])
     end
   end
 
   def delete
     # DELETE /products/:id
-    product = Product.find(params["id"]).destroy
-    unless product.errors.messages.any?
-      render status: 204
-    end
+    product = Product.find(params['id']).destroy
+    render status: 204 unless product.errors.messages.any?
   end
 end
