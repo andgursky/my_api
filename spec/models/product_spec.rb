c59fe1ff22f2c15eabe468207a48f7d8b9d4542d
Andrey Gursky <andgursky@gmail.com>
2018-02-20 18:31:26 +0200

    Product - model was covered by specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "Model" do
+    before :each do
+      allow_any_instance_of(described_class).to receive(:non_zero_price)
+        .and_return true
+      allow(subject).to receive(:set_current_product_count).and_return true
+    end
+  end
+  it { is_expected.to validate_numericality_of(:price) }
+  it { is_expected.to belong_to(:category) }
 end
